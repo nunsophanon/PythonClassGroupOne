3826bac17e0af912794546cdc472cdf35b14d28a
nunsophanon <sophanonnun07@gmail.com>
2021-09-15 20:52:34 +0700

    Indexes and Slicing

---
+++ indexes.py
@@ -0,0 +1,29 @@
+# indexes.py
+
+# l = [1,2,3,4,5,6,7,8]
+# Pirnt forward
+# print("List L ", l[1])
+
+#Print Backward
+# print("List L ", l[-1])
+# print("Old List= ", l)
+# l[4] = 9
+# print("New List= ", l)
+
+# length = len(l)
+# print(length)
+
+l = [
+	[1,2,3],
+	[4,5,6],
+	[7,8,9]
+
+	 ]
+
+# l[2][0] = 10
+print(l[2][1])
+
+
+
+
+
