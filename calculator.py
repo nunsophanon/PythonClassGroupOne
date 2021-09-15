206d3faf75317528e79ab8b10382e8d2aef7cfe0
Nun Sophanon <sophanonnun07@gmail.com>
2021-09-15 19:58:43 +0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print("How many numbers do you want to calculate?")
+
+n = int(input())
+sum = 0
+multiple = 0
+
+for i in range(0,n):
+  print("Inter your number ", i)
+  num = int(input())
+  sum =+ num
+  multiple = multiple * num
+ 
+print()
