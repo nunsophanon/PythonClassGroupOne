55dc9382addcc871088cfa491646dfd7a5b9e949
nunsophanon <sophanonnun07@gmail.com>
2021-09-23 20:46:53 +0700

    Python Class Day 7

---
+++ quotes_in_python.py
@@ -0,0 +1,21 @@
+# quotes_in_python.py
+
+#Single quote
+word = 'Cat'
+# print(word)
+name = 'My Name is "Sithy"'
+# print(name)
+# sentence = 'You're beautiful'
+
+#Double Quote
+code = "Hello World!"
+# code = 'Hello World!'
+# print(code)
+sentence1 = "You're beautiful"
+# print(sentence1)
+sentence2 = 'He asked, "did you speak with him?"'
+
+#Triple Quote
+sentence3 = '''You're talking, "Who's there?"'''
+
+print(sentence3)
---
+++ sting_concatenation.py
@@ -0,0 +1,18 @@
+# sting_concatenation.py
+
+name = 'My name is Leang Y'
+
+age = 'I am 20 years old.'
+
+phrases = name + ', '+age
+
+# print(phrases)
+
+names = ["Menghy", "LEANG Y", "Sithy", "Menglot", "Sokhang", "LiiFong", "Thornin", "Kiri"]
+
+for name in names:
+
+	sentence = ' '.join(['Hello there ', name, ', ', 'How are you?'])
+print(sentence)
+
+
